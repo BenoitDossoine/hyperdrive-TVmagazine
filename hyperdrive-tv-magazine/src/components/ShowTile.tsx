d785f691f5af994a3c279192254a63af4ebbd17a
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-03-31 14:20:44 +0200

    Styling update

--- hyperdrive-tv-magazine/src/components/ShowTile.tsx
+++ hyperdrive-tv-magazine/src/components/ShowTile.tsx
@@ -6,12 +6,14 @@ function ShowTile(props:Props){
       <div className="showContainer">
         {props.show.show.image?.medium && <img src={props.show.show.image.medium} alt="" className="showImg" />}
       <div className="showInfo">
-        <h1 className="episodeTitle">{props.show.name}</h1>
+        <div className="infoHeader">
+          <h1 className="episodeTitle">{props.show.name}</h1>
+          <p className="airtime">{props.show.airtime}</p>
+        </div>
         <h2 className="showName">{props.show.show.name}</h2>
         <p className="showType">{props.show.show.type}</p>
-        <p className="airtime">{props.show.airtime}</p>
         <div className="showDescription" dangerouslySetInnerHTML={{ __html: props.show.show.summary}}></div>
-        <button className="episodeBtn">
+        <button className="tileBtn">
           <Link className="episodeLink" to={`/show/${props.show.show.id}/${props.show.season}/${props.show.number}`}>More info</Link>
         </button>
       </div>
